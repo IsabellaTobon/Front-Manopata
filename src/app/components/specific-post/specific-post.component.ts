e18a104ddd9391da804c4a63253883059f6b6ad4
Isabella T. <isabellatobon@gmail.com>
2024-07-24 02:13:28 +0200

    Hecha la idea de las adopciones
    No funciona el href, falta color tamaños y lógica

---
+++ src/app/components/specific-post/specific-post.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'specific-post',
+  standalone: true,
+  imports: [],
+  templateUrl: './specific-post.component.html',
+  styleUrl: './specific-post.component.css'
+})
+export class SpecificPostComponent {
+
+}
